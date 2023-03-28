ee7625ec07f380b5fb67b78eac7eaa9ec4163276
Uthra1999 <56947376+Uthra1999@users.noreply.github.com>
2023-03-29 00:20:42 +0530

    Add files via upload

---
+++ mavenproject/src/test/java/com/simple/mavenproject/ParisTest.java
@@ -0,0 +1,12 @@
+package com.simple.mavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ParisTest {
+	@Test
+	public void coreect()
+	{
+        assertTrue( true );
+}}
