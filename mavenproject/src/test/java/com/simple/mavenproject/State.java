87e76ff989ec4a2b81055be2335e5696d6a722c1
Uthra1999 <56947376+Uthra1999@users.noreply.github.com>
2023-03-30 11:59:44 +0530

    Add files via upload

---
+++ mavenproject/src/test/java/com/simple/mavenproject/State.java
@@ -0,0 +1,18 @@
+package com.simple.mavenproject;
+
+public class State {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a = 4;
+      int b = 4;
+      int c = a+b;
+      int d = 8; 
+      assert c == d;
+      System.out.println(c);
+      
+    		
+
+	}
+
+}
